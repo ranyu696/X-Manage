8dc5af0043a733014fbac5d99a9420c5bcc87fcf
Ran Yu <ranyu696@gmail.com>
2025-12-06 11:03:51 +0800

    Initial Commit

---
+++ X-Manage/X_ManageApp.swift
@@ -0,0 +1,32 @@
+//
+//  X_ManageApp.swift
+//  X-Manage
+//
+//  Created by xiaoxin on 12/6/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct X_ManageApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ X-ManageTests/X_ManageTests.swift
@@ -0,0 +1,17 @@
+//
+//  X_ManageTests.swift
+//  X-ManageTests
+//
+//  Created by xiaoxin on 12/6/25.
+//
+
+import Testing
+@testable import X_Manage
+
+struct X_ManageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
